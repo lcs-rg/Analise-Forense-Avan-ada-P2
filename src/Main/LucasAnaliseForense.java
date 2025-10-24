4e184d7c191e24ef55e7479d86b2379923d394ce
lcs-rg <lucasrochag9@gmail.com>
2025-10-24 03:34:07 -0300

    Desafio 2 concluído

--- src/Main/LucasAnaliseForense.java
+++ src/Main/LucasAnaliseForense.java
@@ -14,18 +14,37 @@ public class LucasAnaliseForense implements AnaliseForenseAvancada {
 
     @Override
     public Set<String> encontrarSessoesInvalidas(String arquivo) throws IOException {
-        Map<>
+        LinkedHashMap<String, Stack>
         // Implementar usando Map<String, Stack<String>>
     }
 
     @Override
     public List<String> reconstruirLinhaTempo(String arquivo, String sessionId) throws IOException {
         // Implementar usando Queue<String>
+        Queue<String> timeline = new ArrayDeque<>();//Deque para enfileirar histórico criada
+        List<String> result = new ArrayList<>();//ArrayList do histórico final
+            try (BufferedReader br = new BufferedReader(new FileReader(arquivo), 1024 * 1024)){//buffer com leitura de 1MB
+                br.readLine();//lê cabeçalho
+                String linha;
+                while ((linha = br.readLine()) != null) {//loop para ler todas as linhas
+                    String[] campos = linha.split(",");//separa os campos do log
+                    if (campos[2].equals(sessionId)){//caso sessionId seja igual, adiciona a fila
+                        timeline.add(campos[4]);
+                    }
+                }
+                //desenfileirando da deque e enfileirando no arrayList final
+                while (!timeline.isEmpty()) {
+                    result.add(timeline.poll());
+                }
+            }
+                        return result.isEmpty() ? Collections.emptyList() : result;
+
     }
 
     @Override
     public List<Alerta> priorizarAlertas(String arquivo, int n) throws IOException {
         // Implementar usando PriorityQueue<Alerta>
+        List<String> alertas = new PriorityQueue<>();
     }
 
     @Override
